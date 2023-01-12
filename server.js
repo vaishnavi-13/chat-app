8fe3b1fa3c70bc33fbe14993a898f9345e9221b6
vaishnavi-13 <vaishnavi.bhadrashette@gmail.com>
2023-01-12 17:42:35 +0530

    testing comment added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const messages = require("./routes/api/messages");
 
 const app = express();
 
-// Port that the webserver listens to
+// Port that the webserver listens to 5000
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () =>
